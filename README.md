3c99cbd0f39b4747fcb5a2e821ec10d924e4e2aa
hzy1919 <zhiyong198975@126.com>
2021-10-10 23:19:48 +0800

    update README and clear some comments.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Example Subgraph
+# PNS(Polkadot Name System) Subgraph
 
-An example to help you get started with The Graph. For more information see the docs on https://thegraph.com/docs/.
+A subgraph to get PNS data from Etheruem compatible blockchain(like Etheruem,Moonbeam.) with The Graph . 
--- src/mapping-ENS.ts
+++ src/mapping-ENS.ts
@@ -1,24 +1,4 @@
 
-// Import types and APIs from graph-ts
-// import {
-//   ByteArray,
-//   crypto,
-//   ens
-// } from '@graphprotocol/graph-ts'
-
-
-// // Import event types from the registry contract ABI
-// import {
-//   NameRegistered as NameRegisteredEvent,
-// } from './types/Registrar/Registrar'
-
-// import {
-//   NewSubnameOwner as NewSubnameOwnerEvent,
-// } from './types/ENS/ENSRegistry'
-
-// // Import entity types generated from the GraphQL schema
-// import { Domains, Subdomains } from './types/schema'
-
 import { NewSubnameOwner as NewSubnameOwnerEvent } from '../generated/Subdomain/ENS'
 import { Subdomain } from '../generated/schema'
 
